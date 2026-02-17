28cc3a3cd48f6d246fb92733854cdb1e0489a3c5
Choudharyakshat711 <choudharyakshat711@gmail.com>
2026-02-17 13:09:54 +0530

    Modification in Controller

--- src/main/java/com/ecommerce/products/controller/ProductAdminController.java
+++ src/main/java/com/ecommerce/products/controller/ProductAdminController.java
@@ -1,15 +1,11 @@
 package com.ecommerce.products.controller;
 
-import com.ecommerce.products.dto.ProductDto;
 import com.ecommerce.products.entity.Product;
 import com.ecommerce.products.service.ProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
@@ -23,4 +19,17 @@ public class ProductAdminController {
          productService.addProduct(productDto);
          return new ResponseEntity<>(HttpStatus.OK);
     }
+
+    @PostMapping("/update")
+    public ResponseEntity<Void> updateProduct(@RequestBody Product product){
+        productService.updateProduct(product);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<Void> deleteProduct(@PathVariable Long id){
+        productService.deleteProduct(id);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
 }
--- src/main/java/com/ecommerce/products/controller/ProductUserController.java
+++ src/main/java/com/ecommerce/products/controller/ProductUserController.java
@@ -6,16 +6,14 @@ import com.ecommerce.products.service.ProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("products/user")
+@CrossOrigin(origins = "http://localhost:5173")
 public class ProductUserController {
 
     private final ProductService productService;
